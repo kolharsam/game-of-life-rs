8e2242d1d551057da5acd24181595cb2d678d4f8
Sameer Kolhar <kolhar730@gmail.com>
2021-02-22 21:13:27 +0530

    update main mod

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,15 @@
-use clap::{load_yaml, App};
 use crossterm::{
     execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
     Result,
 };
 use std::io::stdout;
-
-// All known options on the CLI
-static ROWS: &str = "rows";
-static COLUMNS: &str = "columns";
+mod cli;
+mod game;
+mod utils;
 
 fn main() -> Result<()> {
-    let yaml = load_yaml!("cli.yaml");
-    let matches = App::from(yaml).get_matches();
-    let mut grid_rows = "50";
-    let mut grid_cols = "50";
-
-    if let Some(i) = matches.value_of(ROWS) {
-        grid_rows = i;
-        println!("Value for rows: {}", i);
-    }
-    if let Some(i) = matches.value_of(COLUMNS) {
-        grid_cols = i;
-        println!("Value for columns: {}", i);
-    }
-
+    let (grid_rows, grid_cols) = cli::load_game();
     execute!(
         stdout(),
         SetForegroundColor(Color::Blue),
