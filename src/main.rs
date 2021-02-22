4ac44952c4aea3147f58748ef7e5e8e9e9e72d05
Sameer Kolhar <kolhar730@gmail.com>
2021-02-22 09:58:57 +0530

    update main with basic CLI setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, Write};
+use clap::{App, load_yaml};
+use crossterm::{
+    event, execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand, Result,
+};
+
+// All known options on the CLI
+static ROWS: &str = "rows";
+static COLUMNS: &str = "columns";
+
+fn main() -> Result<()> {
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+    let mut grid_rows = "50";
+    let mut grid_cols = "50";
+    
+    if let Some(i) = matches.value_of(ROWS) {
+        grid_rows = i;
+        println!("Value for rows: {}", i);
+    }
+    if let Some(i) = matches.value_of(COLUMNS) {
+        grid_cols = i;
+        println!("Value for columns: {}", i);
+    }
+    
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Green),
+        Print(format!("You have selected {} rows and {} columns", grid_rows, grid_cols)),
+        ResetColor
+    )?;
+
+    Ok(())
 }
