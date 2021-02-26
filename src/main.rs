d90c9a63358017ce9769d0792f731d790da1264e
Sameer Kolhar <kolhar730@gmail.com>
2021-02-27 00:16:04 +0530

    update main with some trial for "animation"

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
 use crossterm::{
     execute,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    style::Print,
+    terminal::{Clear, ClearType},
     Result,
 };
 use std::io::stdout;
+use std::{thread, time};
 mod cli;
 mod game;
 mod utils;
 
+const ONE_SECOND: time::Duration = time::Duration::from_secs(1);
+
 fn main() -> Result<()> {
     let (grid_rows, grid_cols) = cli::load_game();
-    execute!(
-        stdout(),
-        SetForegroundColor(Color::Blue),
-        SetBackgroundColor(Color::Green),
-        Print(format!(
-            "You have selected {} rows and {} columns",
-            grid_rows, grid_cols
-        )),
-        ResetColor
-    )?;
+
+    let new_start_set = utils::randomize_state(grid_rows, grid_cols);
+
+    let mut number = 0;
+    let mut number2 = 100;
+    loop {
+        execute!(
+            stdout(),
+            Clear(ClearType::All),
+            Print(format!(
+                "\t\t{}------{:?}-----{}\n",
+                number, new_start_set, number2
+            )),
+            // MoveToPreviousLine(1)
+        )?;
+        number += 1;
+        number2 -= 2;
+        if number == 25 {
+            break;
+        }
+        thread::sleep(ONE_SECOND);
+    }
 
     Ok(())
 }
