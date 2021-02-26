e391504cf5274935bc9f33c96bf50aa33fdd45a3
Sameer Kolhar <kolhar730@gmail.com>
2021-02-27 00:14:56 +0530

    update game structures

--- src/game.rs
+++ src/game.rs
@@ -1,17 +1,15 @@
 use std::collections::HashSet;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
-pub struct Coordinate {
-    x: u32,
-    y: u32,
-}
+// notation is (x, y);
+pub struct Coordinate(pub u32, pub u32);
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Grid {
-    width: u32,
-    height: u32,
+    pub width: u32,
+    pub height: u32,
     // HashSet - because we can have a lower memory
     // footprint as we'll be storing alive cells only
     // and also that operations in the game become easier
-    cells: HashSet<Coordinate>,
+    pub cells: HashSet<Coordinate>,
 }
