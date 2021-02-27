10d893d85c317bde21f7b1c3f11173f941f2545a
Sameer Kolhar <kolhar730@gmail.com>
2021-02-27 22:28:14 +0530

    update game mod with new impl for Grid

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,4 @@
+use crate::utils;
 use std::collections::HashSet;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
@@ -6,10 +7,34 @@ pub struct Coordinate(pub u32, pub u32);
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Grid {
-    pub width: u32,
-    pub height: u32,
+    pub rows: u32,
+    pub columns: u32,
     // HashSet - because we can have a lower memory
     // footprint as we'll be storing alive cells only
     // and also that operations in the game become easier
     pub cells: HashSet<Coordinate>,
+}
+
+impl Grid {
+    pub fn new(rows: u32, columns: u32) -> Self {
+        Grid {
+            rows,
+            columns,
+            cells: utils::randomize_state(rows, columns),
+        }
+    }
+    pub fn print_grid_state(&self, r: u32) -> String {
+        let mut final_str: String = String::new();
+        let row = format!(
+            "\t\t\t\tYou have {} rows and {} columns\n",
+            self.rows * r,
+            self.columns * r
+        );
+
+        for _i in 0..=10 {
+            final_str += &row;
+        }
+
+        final_str
+    }
 }
