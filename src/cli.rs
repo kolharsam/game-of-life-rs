ed80ae4ed8e291f0b9eb84ea054f15939ce86cbf
Sameer Kolhar <kolhar730@gmail.com>
2021-02-27 22:28:45 +0530

    update cli mod by using the new utils

--- src/cli.rs
+++ src/cli.rs
@@ -2,31 +2,31 @@ use crate::utils;
 use clap::{load_yaml, App};
 
 // All known options on the CLI
-static ROWS: &str = "rows";
-static COLUMNS: &str = "columns";
-static DELAY: &str = "delay";
+const ROWS: &str = "rows";
+const COLUMNS: &str = "columns";
+const DELAY: &str = "delay";
+const STATES: &str = "states";
 
-pub fn load_game() -> (u32, u32, u32) {
+// All Default states for the cli flags
+const DEFAULT_ROWS_COLUMNS: &str = "25";
+const DEFAULT_TIME_DELAY: &str = "1";
+const DEFAULT_STATE_ITERS: &str = "20";
+
+pub type CLIInputs = (u32, u32, u32, u32);
+
+pub fn load_game_info() -> CLIInputs {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from(yaml).get_matches();
 
-    let mut grid_rows = "50";
-    let mut grid_cols = "50";
-    let mut time_delay = "1";
+    let parsed_grid_rows = utils::parse_cli_flag(&matches, ROWS, DEFAULT_ROWS_COLUMNS);
+    let parsed_grid_cols = utils::parse_cli_flag(&matches, COLUMNS, DEFAULT_ROWS_COLUMNS);
+    let parsed_time_delay = utils::parse_cli_flag(&matches, DELAY, DEFAULT_TIME_DELAY);
+    let parsed_max_states = utils::parse_cli_flag(&matches, STATES, DEFAULT_STATE_ITERS);
 
-    if let Some(i) = matches.value_of(ROWS) {
-        grid_rows = i;
-    }
-    if let Some(i) = matches.value_of(COLUMNS) {
-        grid_cols = i;
-    }
-    if let Some(i) = matches.value_of(DELAY) {
-        time_delay = i;
-    }
-
-    let grid_rows_num = utils::convert_to_u32(grid_rows);
-    let grid_cols_num = utils::convert_to_u32(grid_cols);
-    let time_delay_num = utils::convert_to_u32(time_delay);
-
-    (grid_rows_num, grid_cols_num, time_delay_num)
+    (
+        parsed_grid_rows,
+        parsed_grid_cols,
+        parsed_time_delay,
+        parsed_max_states,
+    )
 }
