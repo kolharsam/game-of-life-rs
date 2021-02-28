0a70250c8a77027015a72fd086952444b248ad41
Sameer Kolhar <kolhar730@gmail.com>
2021-02-28 10:47:33 +0530

    clean up main based on updates on mod game

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,22 @@ const ONE_SECOND: time::Duration = time::Duration::from_secs(1);
 
 fn main() -> Result<()> {
     let (grid_rows, grid_cols, time_delay, states) = cli::load_game_info();
-    let game_grid = game::Grid::new(grid_rows, grid_cols);
+    let mut game_grid = game::Grid::new(grid_rows, grid_cols);
     let mut counter = 0;
 
     loop {
+        // FIXME?: the output from this may not be ideal
+        // especially the clearing of screen part and the layout
+
+        // TODO: add information like, how many are alive at that
+        // particular point in time / iteration
         execute!(
             stdout(),
             Clear(ClearType::All),
-            Print(game_grid.print_grid_state(counter)),
+            Print(game_grid.print_grid_state()),
             MoveToPreviousLine(1),
         )?;
+
         counter += 1;
         if counter == states {
             break;
